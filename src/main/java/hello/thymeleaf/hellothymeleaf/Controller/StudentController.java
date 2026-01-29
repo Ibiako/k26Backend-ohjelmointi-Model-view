2993c4b0ce5289b7d979487330dba1e6f5defb73
Ibrahim Ako <bho338@myy.haaga-helia.fi>
2026-01-29 14:43:02 +0200

    1: Handling Lists

---
+++ src/main/java/hello/thymeleaf/hellothymeleaf/Controller/StudentController.java
@@ -0,0 +1,24 @@
+package hello.thymeleaf.hellothymeleaf.controller;
+
+import hello.thymeleaf.hellothymeleaf.student.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/students")
+    public String students(Model model) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Kate", "Cole"));
+        students.add(new Student("Dan", "Brown"));
+        students.add(new Student("Mike", "Mars"));
+
+        model.addAttribute("students", students);
+        return "students";
+    }
+}
